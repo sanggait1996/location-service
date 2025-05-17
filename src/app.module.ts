473a44011884245d93f6b58d53b4d96e8caed5d2
Sang Nguyen <sanggait1996@gmail.com>
2025-05-17 16:02:03 +0700

    install and config nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LocationsModule } from './modules/locations/location.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), LocationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    LocationsModule,
+  ],
   controllers: [],
   providers: [],
 })
